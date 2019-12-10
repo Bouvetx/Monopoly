c66a5173102e3b84766e88129e3f8b3fd582a46b
bouvet <bouvet.xavier@hotmail.com>
2019-12-10 14:52:29 +0100

    Add all class

---
+++ Monopoly/Joueur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class Joueur
+    {
+        private int fond;
+        private string pion;
+        private string[] pionList = { "Le Chapeau Haut-de-Forme", "La Voiture", "Le Bateau", "Le Dé à coudre", "La Chaussure", "La Brouette", "Pion chapeau du MonopolyLe Chien", "Le Fer à repasser", "Le Cavalier et son Cheval", "Le Chat", "Pion chat du Monopoly", "Le Sac de Billets", "Le Canon", "La Lanterne", "Le Cheval à Bascule", "Le Sac à Mains" };
+
+
+        public Joueur(string pion)
+        {
+            fond = 20000;
+            pion = this.pion;
+        }
+        public Joueur()
+        {
+            fond = 20000;
+            var rand = new Random(DateTime.Now);
+            pion = pionList[rand.Next(pionList.Length)];
+        }
+    }
+}
