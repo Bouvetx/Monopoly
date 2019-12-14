bf91651516909172fe97300364b024cc6683d5f0
Maxime CANTIE <maxime.cantie@edu.devinci.fr>
2019-12-14 19:58:58 +0100

    last

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -10,6 +10,14 @@ namespace Monopoly
     {
         static void Main(string[] args)
         {
+            // CaseFactory case1 = new ActionFactory("Départ");
+            CaseFactory case2 = new PropFactory("rue des andes", 25, 120);
+            //Case case1bis = case1.GetCreditCard();
+            //Console.WriteLine("Action name: {0}",case1bis.ActionName);
+            Case case3 = case2.GetCase();
+            Console.WriteLine("street: {0}\n rent: {1}\n price: {2}", case3.StreetName, case3.Rent, case3.Price);/*
+
+            Console.WriteLine();
             Plateau P = Plateau.Instance;
             int numJ=0;
             int i = 0;
@@ -29,7 +37,7 @@ namespace Monopoly
                 
                 P.NextJoueur();
                 Console.ReadLine();
-            }
+            }*/
             Console.ReadKey();
         }
     }
