f9653504b41383184a982757e0a83c214ae8d75e
Maxime CANTIE <maxime.cantie@edu.devinci.fr>
2019-12-15 17:19:54 +0100

    commit observer

---
+++ Monopoly/JoueurObserver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class JoueurObserver : IJoueur
+    {
+        private string pion;
+        private Observer observ;
+
+        public JoueurObserver(string pion)
+        {
+            this.pion = pion;
+        }
+
+        public void Update(Observer observ)
+        {
+            Console.WriteLine("Notified "+pion+" of "+ observ.GetType().Name+" "+observ.Ownership);
+        }
+    }
+}
